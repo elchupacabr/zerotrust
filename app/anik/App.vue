836557d73dbfcb43151ca7f41ff434af600e0076
flyandflake <90207912+flyandflake@users.noreply.github.com>
2022-12-07 18:40:58 +0300

    Create App.vue

---
+++ app/anik/App.vue
@@ -0,0 +1,11 @@
+<notifications group="foo" />
+ 
+<button class="btn" @click="show('foo')">click me</button>
+ 
+<script>
+this.$notify({
+  group: 'foo',
+  title: 'Important message',
+  text: 'Hello user! This is a notification!'
+});
+</script>
